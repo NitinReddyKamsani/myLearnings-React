c7436a325ef0283d829ecc88bd41d6206d466cae
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-04-11 18:21:46 +0530

    Updated Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,41 @@
-const Contact = () =>{
-    return (
+import React from "react";
+
+class Contact extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            userInfo : {
+                name : "dummmy",
+
+            }
+        };
+    };
+
+    async componentDidMount(){
+        
+        const data = await fetch("https://api.github.com/users/NitinReddyKamsani")
+        const json = await data.json();
+        this.setState({
+            userInfo : json,
+        })
+        console.log(json);
+    };
+
+   
+
+    render(){
+
+        const {name} = this.state.userInfo
+
+        return(
         <div>
-            Contact
+            <h2>Name : {name}</h2>
+            <h2>Email : supportfoodvilla@icloud.com</h2>
         </div>
-    )
+        )
+    }
 }
 
 export default Contact;
