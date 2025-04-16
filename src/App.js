e6c0771b650f4b7a934bc0a20df87ec2831c4adb
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-04-16 21:54:39 +0530

    iImplementing useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{lazy, Suspense} from "react";
+import React,{lazy, Suspense, useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
 import Header from "./components/Header";
@@ -8,17 +8,30 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Shimmer from "./components/Shimmer";
+import UserContext from "../utils/UserContext";
 //import Grocery from "./components/Grocery";
 
 
 const Grocery = lazy(()=> import("./components/Grocery"))
 
 const AppLayout = () => {
+
+const [userName,setUserName] = useState();
+
+useEffect(()=>{
+  const data={
+    name : "Nitin Reddy Kamsani",
+  }
+  setUserName(data.name);
+},[])
+
   return (
+    <UserContext.Provider value={{loggedInUser : userName}}>
     <div className="App">
       <Header />
       <Outlet />
     </div>
+    </UserContext.Provider>
   );
 };
 
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -9,13 +9,15 @@ const ItemList = ({items}) =>{
                         <div className="w-9/12">
                             <div className="py-2 ">
                             <span>{item.card.info.name}</span>
-                            <span>-₹{item.card.info.price/100}</span>
+                            <span>-₹{item.card.info.price/100 || item.card.info.defaultPrice/100}</span>
                             
                             </div>
                             <p className="text-xs">{item.card.info.description}</p>
                         </div>
-                        <div className="w-3/12 p-4">
+                        <div className="w-3/12 p-4 relative">
                             <img className="rounded-md" src={CDN_URL + item.card.info.imageId} />
+                            <button className="bg-black text-white absolute mx-[40px] rounded-lg p-1 bottom-3">Add +</button>
+                             
                         </div>
                     </div>
                 ))
