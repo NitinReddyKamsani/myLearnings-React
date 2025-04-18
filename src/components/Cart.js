2ccff4f463f41866377ce66fa0b6da21dea709ae
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-04-18 21:13:28 +0530

    implementing cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,39 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CDN_URL } from "../../utils/constants";
+import { clearCart, removeItem } from "../../utils/cartSlice";
 
 const Cart = () =>{
 
+    const dispatch  = useDispatch()
+
+    function handleClearCart(){
+        dispatch(clearCart())
+    }
+
+    const handleRemove = () => {
+        dispatch(removeItem())
+    }
+    
+    
     const cartItems = useSelector((store)=>store.cart.Items);
-    console.log(cartItems);
 
     return (
+
+        
+
+        cartItems.length === 0 ? <h1 className="font-bold text-2xl m-48 text-center">Cart is Empty Add Items !!! </h1> : 
+
         <>
         <h1 className="font-bold  text-center text-2xl m-4 p-4">Cart</h1>
+        <div className="flex justify-end w-9/12">
+        <button className="p-2 bg-gray-200 rounded-lg" onClick={handleClearCart}>Clear Cart</button>
+        </div>
         <div className="p-4 w-6/12 bg-gray-200 mx-auto my-4 rounded-md bottom-4 shadow-xl">  
-           {
+           {   
             cartItems.map((item,index)=>(
                 <div key={index} >
                     <div className="flex justify-between items-start border-b border-gray-300 py-4"> 
+    
                         <div className="w-9/12 text-left">
                             <div className="py-1">
                                 <span>{item?.card?.info?.name}</span>
@@ -22,15 +42,21 @@ const Cart = () =>{
 
                         <p className="text-xs w-9/12 py-2">{item?.card?.info?.description}</p>
                         </div>
-                    <div className="3/12 relative">
+                    <div className="3/12 relative flex flex-col">
                         <img className="rounded-md h-28 w-24" src={CDN_URL + item.card.info.imageId} />
+                        <button className="py-2 rounded-lg bg-slate-200" onClick={handleRemove}>Remove</button>
                     </div>
                     </div>
+                  
                 </div>
+                
             ))
+            
            }
         </div>
+        
         </>
+        
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -24,12 +24,12 @@ const Header = () =>{
             </div>
             <div className="flex">
                 <ul className="flex gap-3 p-6 m-7">
-                    <Link to="/" className="link"> <li>Home</li></Link>
-                    <Link to="/about" className="link" > <li>About US</li> </Link>  
-                    <Link to="/contact" className="link"> <li>Contact US</li> </Link>
-                    <Link to="/cart" className="link"><li>Cart-({cartItems.length})</li></Link>
-                    <Link to = "/grocery" className="link"><li>Grocery</li></Link>
-                    <Link className="link"><li>{loggedInUser}</li></Link>
+                    <Link to="/" className="link"> <li className="hover:bg-slate-200">Home</li></Link>
+                    <Link to="/about" className="link" > <li className="hover:bg-slate-200">About US</li> </Link>  
+                    <Link to="/contact" className="link"> <li className="hover:bg-slate-200">Contact US</li> </Link>
+                    <Link to="/cart" className="link"><li className="hover:bg-slate-200">Cart-({cartItems.length})</li></Link>
+                    <Link to = "/grocery" className="link"><li className="hover:bg-slate-200">Grocery</li></Link>
+                    <Link className="link"><li>{loggedInUser}</li ></Link>
                     <button className="border-black bg-black text-white p-1 bottom-2 rounded-sm" onClick={handleBtn}>{Btn}</button>
                 </ul>
             </div>
